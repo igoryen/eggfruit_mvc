18b30a9a223a2d22a0ab5bc9d07010fc224f3909
igoryen <igor.yentaltsev@gmail.com>
2015-01-05 13:39:28 -0500

    Can display all rows from tbl_entry

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('/', function(){
+  $applications = DB::table('tbl_entry')->get(); // 1
+  return $applications;
 });
