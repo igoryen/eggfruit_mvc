d75f1fe0ddf9094bf3520246a694059a6665662a
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-16 12:35:20 -0500

    200216-1235 - ignore, just formatting

--- app/views/applications/index.blade.php
+++ app/views/applications/index.blade.php
@@ -8,7 +8,7 @@
 <php?
 
 ?>
-  <div class="panel">
+<div class="panel">
     <h1>My applications ({{ $total }})</h1>
 
     <div class="today">Today <span class="hazy">({{ $today }})</span>: <span class="todays-number">{{ $asoftoday }}</span></div>
@@ -17,160 +17,160 @@
     <div class="refusals">Ignores: {{ $ignores }} <<< FIX THIS!!!</div>
     <div class="interviews">In-Person Interviews: {{ $interviews17 }}</div>
 
-  </div>
+</div>
 
-  <div class="table">
+<div class="table">
     @if($applications->count())
-      <table class="applied">  
+        <table class="applied">  
         <thead>
-          <tr>
-            <th></th>
-            
-            <th>
-              @if($sortby == 'company' && $order == 'asc'){{
-                link_to_action(
-                  'ApplicationsController@index',
-                  'Company',
-                  array(
-                    'sortby' => 'company_name',
-                    'order' => 'desc'
-                  )
-                )
-              }}
-              @else{{
-                link_to_action(
-                  'ApplicationsController@index',
-                  'Company',
-                  array(
-                    'sortby' => 'company_name',
-                    'order' => 'asc'
-                  )
-                )
-              }}
-              @endif
-            </th>
-            <th>
-              @if($sortby == 'company' && $order == 'asc'){{
-                link_to_action(
-                  'ApplicationsController@index',
-                  'Position',
-                  array(
-                    'sortby' => 'position_name',
-                    'order' => 'desc'
-                  )
-                )
-              }}
-              @else{{
-                link_to_action(
-                  'ApplicationsController@index',
-                  'Position',
-                  array(
-                    'sortby' => 'position_name',
-                    'order' => 'asc'
-                  )
-                )
-              }}
-              @endif
-            </th>
-            <th>Recruiter</th>
-            <th>
-              @if($sortby == 'date' && $order == 'desc'){{
-                link_to_action(
-                  'ApplicationsController@index',
-                  'Applied',
-                  array(
-                    'sortby' => 'applied_date',
-                    'order' => 'asc'
-                  )
-                )
-              }}
-              @else{{
-                link_to_action(
-                  'ApplicationsController@index',
-                  'Applied',
-                  array(
-                    'sortby' => 'applied_date',
-                    'order' => 'desc'
-                  )
-                )
-              }}
-              @endif
-            </th>
-            <th>Phone Interview</th>
-            <th>Interview</th>
-            <th>Response Date</th>
-            <th>Response Value</th>
-            <th>Accepted?</th>
-          </tr>
+            <tr>
+                <th></th>
+
+                <th>
+                    @if($sortby == 'company' && $order == 'asc'){{
+                        link_to_action(
+                            'ApplicationsController@index',
+                            'Company',
+                            array(
+                                'sortby' => 'company_name',
+                                'order' => 'desc'
+                            )
+                        )
+                    }}
+                    @else{{
+                        link_to_action(
+                            'ApplicationsController@index',
+                            'Company',
+                            array(
+                                'sortby' => 'company_name',
+                                'order' => 'asc'
+                            )
+                        )
+                    }}
+                    @endif
+                </th>
+                <th>
+                    @if($sortby == 'company' && $order == 'asc'){{
+                        link_to_action(
+                            'ApplicationsController@index',
+                            'Position',
+                            array(
+                                'sortby' => 'position_name',
+                                'order' => 'desc'
+                            )
+                        )
+                    }}
+                    @else{{
+                        link_to_action(
+                            'ApplicationsController@index',
+                            'Position',
+                            array(
+                                'sortby' => 'position_name',
+                                'order' => 'asc'
+                            )
+                        )
+                    }}
+                    @endif
+                </th>
+                <th>Recruiter</th>
+                <th>
+                    @if($sortby == 'date' && $order == 'desc'){{
+                        link_to_action(
+                            'ApplicationsController@index',
+                            'Applied',
+                            array(
+                                'sortby' => 'applied_date',
+                                'order' => 'asc'
+                            )
+                        )
+                    }}
+                    @else{{
+                        link_to_action(
+                            'ApplicationsController@index',
+                            'Applied',
+                            array(
+                                'sortby' => 'applied_date',
+                                'order' => 'desc'
+                            )
+                        )
+                    }}
+                    @endif
+                </th>
+                <th>Phone Interview</th>
+                <th>Interview</th>
+                <th>Response Date</th>
+                <th>Response Value</th>
+                <th>Accepted?</th>
+            </tr>
         </thead>
         <tbody>
-      @foreach($applications as $application)
-        <?php
-          $date_today = date_create(date("Y-m-d"));
-          $date_apply = date_create($application->applied_date);
-          $time_since = date_diff($date_today, $date_apply);
-        ?>
+            @foreach($applications as $application)
+            <?php
+            $date_today = date_create(date("Y-m-d"));
+            $date_apply = date_create($application->applied_date);
+            $time_since = date_diff($date_today, $date_apply);
+            ?>
 
-        @if( $time_since->days > 40 && $application->accepted != "0" )
-          <tr class="crossed_row">
-        @elseif( $application->accepted == "0" )
-          <tr class="rejected_row">
-        @else 
-          <tr>
-        @endif
+            @if( $time_since->days > 40 && $application->accepted != "0" )
+            <tr class="crossed_row">
+                @elseif( $application->accepted == "0" )
+                <tr class="rejected_row">
+                    @else 
+                    <tr>
+                        @endif
 
-        <td class="edit">{{ link_to_route(
-                  'applications.edit', 
-                  'Edit',
-                  array($application->id), 
-                  array('class' => 'btn btn-info')) 
-        }}</td>
-        
-        <td class="company">
-                    {{ link_to("{$application->company_url}",
-                    $application->company_name,
-                    array( 'class'=> 'company_url', 'target'=>'blank')
-                    ) 
-            }}</td> 
+                        <td class="edit">{{ link_to_route(
+                            'applications.edit', 
+                            'Edit',
+                            array($application->id), 
+                            array('class' => 'btn btn-info')) 
+                        }}</td>
 
-        <td>
-                    {{ link_to("{$application->job_posting_url}",
-                    $application->position_name,
-                    array( 'class'=> 'job_posting_url', 'target'=>'blank')
-                    )
-            }}</td>
-        <td>{{ $application->recruiter }}</td>
-        <td class="date">{{ $application->applied_date }}</td>
-        <td class="interview">
-          @if( $application->phone_interview_date != "0000-00-00") 
-            {{ $application->phone_interview_date }}
-          @endif
-        </td>
-        <td class="interview">
-          @if( $application->interview_date != "0000-00-00") 
-            {{ $application->interview_date }}
-          @endif
-        </td>
-        <td class="response date">
-          @if( $application->response_date != "0000-00-00") 
-            {{ $application->response_date }}
-          @endif
-        </td>
-        <td>{{ $application->response_value }}</td>
-        <td>{{ $application->accepted }}</td>
-      </tr>
-      @endforeach
-        </tbody>
-      </table>
-    @else
-      <li>
-        Unfortunately, there are no applications.
-      </li>
-    @endif
-  </div>
+                        <td class="company">
+                            {{ link_to("{$application->company_url}",
+                                $application->company_name,
+                                array( 'class'=> 'company_url', 'target'=>'blank')
+                            ) 
+                        }}</td> 
 
-@stop
+                        <td>
+                            {{ link_to("{$application->job_posting_url}",
+                                $application->position_name,
+                                array( 'class'=> 'job_posting_url', 'target'=>'blank')
+                            )
+                        }}</td>
+                        <td>{{ $application->recruiter }}</td>
+                        <td class="date">{{ $application->applied_date }}</td>
+                        <td class="interview">
+                            @if( $application->phone_interview_date != "0000-00-00") 
+                            {{ $application->phone_interview_date }}
+                            @endif
+                        </td>
+                        <td class="interview">
+                            @if( $application->interview_date != "0000-00-00") 
+                            {{ $application->interview_date }}
+                            @endif
+                        </td>
+                        <td class="response date">
+                            @if( $application->response_date != "0000-00-00") 
+                            {{ $application->response_date }}
+                            @endif
+                        </td>
+                        <td>{{ $application->response_value }}</td>
+                        <td>{{ $application->accepted }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @else
+            <li>
+                Unfortunately, there are no applications.
+            </li>
+            @endif
+        </div>
 
-@section('footer')
-  <h4>This is my footer</h4>
-@stop
+        @stop
+
+        @section('footer')
+        <h4>This is my footer</h4>
+        @stop
